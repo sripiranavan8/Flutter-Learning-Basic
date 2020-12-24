c0f87f20d74b1310bbb9bf8ea0b8cb51035dab23
Sripiranavan Yogarajah <sripiranavan08@gmail.com>
2020-12-24 22:10:53 +0530

    Basic phase 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favorite color?',
+      'What\s your favorite animal?'
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(questions.elementAt(_questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 choosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // ...
+                print('Answer 3 choosen');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
